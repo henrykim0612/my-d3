de2e464bee86735ecb495eefc3eb12ec90d56dea
Henry.Kim <hyeongjong90@gmail.com>
2020-06-26 21:44:38 +0900

    코드 정리

--- src/main/webapp/js/d3-exam/ch5/pie.js
+++ src/main/webapp/js/d3-exam/ch5/pie.js
@@ -40,7 +40,6 @@ const main = (function () {
   function change(key) {
 
     const changingKey = !!key ? key : 'numTweets'; // 기본값은 numTweets
-
     const pieChart = d3.layout.pie().sort(null).value(function(d) {return d[changingKey];});
     const extent = d3.extent(_data, function(d) {return d[changingKey];});
     const colorScale = d3.scale.category10(extent);
@@ -49,105 +48,111 @@ const main = (function () {
     const outerArc = d3.svg.arc().outerRadius(radius * 0.9).innerRadius(radius * 0.9);
     const dataKey = function(d) { return d.data.key; };
 
-    // Slices
-    const slice = _pieG.select('g.sliceG')
-      .selectAll('path.slice')
-      .data(pieChart(_data), dataKey);
+    createSlices() // Slices
+    createLabels() // Labels
+    createPolyLines() // PolyLines
 
-    slice
-      .enter()
-      .append('path')
-      .attr('class', 'slice')
-      .style('fill', function(d, i) { return colorScale(i); });
 
-    slice
-      .transition()
-      .duration(1000)
-      .attrTween("d", function(d) {
-        this._current = this._current || d;
-        const interpolate = d3.interpolate(this._current, d);
-        this._current = interpolate(0);
-        return function(t) {
-          return arc(interpolate(t));
-        };
-      })
+    function createSlices() {
+      const slice = _pieG.select('g.sliceG')
+        .selectAll('path.slice')
+        .data(pieChart(_data), dataKey);
 
-    slice.exit().remove();
+      slice
+        .enter()
+        .append('path')
+        .attr('class', 'slice')
+        .style('fill', function(d, i) { return colorScale(i); });
 
-    // Labels
-    const text = _pieG.select('g.labelG')
-      .selectAll('text.label')
-      .data(pieChart(_data.filter(function(d) {
-        return d[changingKey] > 0; // 있는것만 화면에 보여지도록
-      })), dataKey);
+      slice
+        .transition()
+        .duration(1000)
+        .attrTween("d", function(d) {
+          this._current = this._current || d;
+          const interpolate = d3.interpolate(this._current, d);
+          this._current = interpolate(0);
+          return function(t) {
+            return arc(interpolate(t));
+          };
+        })
 
-    text
-      .enter()
-      .append('text')
-      .attr('class', 'label')
-      .attr('dy', '.35em')
-      .text(dataKey);
+      slice.exit().remove();
+    }
 
-    text
-      .transition()
-      .duration(1000)
-      .attrTween("transform", function(d) {
-        this._current = this._current || d;
-        const interpolate = d3.interpolate(this._current, d);
-        this._current = interpolate(0);
-        return function(t) {
-          const d2 = interpolate(t);
-          const pos = outerArc.centroid(d2);
-          pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-          return "translate("+ pos +")";
-        };
-      })
-      .styleTween("text-anchor", function(d){
-        this._current = this._current || d;
-        const interpolate = d3.interpolate(this._current, d);
-        this._current = interpolate(0);
-        return function(t) {
-          const d2 = interpolate(t);
-          return midAngle(d2) < Math.PI ? "start":"end";
-        };
-      });
+    function createLabels() {
+      const text = _pieG.select('g.labelG')
+        .selectAll('text.label')
+        .data(pieChart(_data.filter(function(d) {
+          return d[changingKey] > 0; // 있는것만 화면에 보여지도록
+        })), dataKey);
 
-    text.exit().remove();
+      text
+        .enter()
+        .append('text')
+        .attr('class', 'label')
+        .attr('dy', '.35em')
+        .text(dataKey);
 
-    // PolyLines
-    const polyline = _pieG.select('g.polylineG')
-      .selectAll('polyline.polyline')
-      .data(pieChart(_data.filter(function(d) {
-        return d[changingKey] > 0;
-      })), dataKey);
+      text
+        .transition()
+        .duration(1000)
+        .attrTween("transform", function(d) {
+          this._current = this._current || d;
+          const interpolate = d3.interpolate(this._current, d);
+          this._current = interpolate(0);
+          return function(t) {
+            const d2 = interpolate(t);
+            const pos = outerArc.centroid(d2);
+            pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+            return "translate("+ pos +")";
+          };
+        })
+        .styleTween("text-anchor", function(d){
+          this._current = this._current || d;
+          const interpolate = d3.interpolate(this._current, d);
+          this._current = interpolate(0);
+          return function(t) {
+            const d2 = interpolate(t);
+            return midAngle(d2) < Math.PI ? "start":"end";
+          };
+        });
 
-    polyline
-      .enter()
-      .append('polyline')
-      .attr('class', 'polyline');
+      text.exit().remove();
+    }
 
-    polyline
-      .transition()
-      .duration(1000)
-      .attrTween("points", function(d){
-        this._current = this._current || d;
-        const interpolate = d3.interpolate(this._current, d);
-        this._current = interpolate(0);
-        return function(t) {
-          const d2 = interpolate(t);
-          const pos = outerArc.centroid(d2);
-          pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-          return [arc.centroid(d2), outerArc.centroid(d2), pos];
-        };
-      });
+    function createPolyLines() {
+      const polyline = _pieG.select('g.polylineG')
+        .selectAll('polyline.polyline')
+        .data(pieChart(_data.filter(function(d) {
+          return d[changingKey] > 0;
+        })), dataKey);
 
-    polyline.exit().remove();
+      polyline
+        .enter()
+        .append('polyline')
+        .attr('class', 'polyline');
 
+      polyline
+        .transition()
+        .duration(1000)
+        .attrTween("points", function(d){
+          this._current = this._current || d;
+          const interpolate = d3.interpolate(this._current, d);
+          this._current = interpolate(0);
+          return function(t) {
+            const d2 = interpolate(t);
+            const pos = outerArc.centroid(d2);
+            pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+            return [arc.centroid(d2), outerArc.centroid(d2), pos];
+          };
+        });
+
+      polyline.exit().remove();
+    }
 
     function midAngle(d){ // 내부 함수로 존재해야 함
       return d.startAngle + (d.endAngle - d.startAngle)/2;
     }
-
   }
 
   return {
